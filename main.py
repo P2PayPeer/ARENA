ebc0ee2dcdd1d6708ed83aecffb3252ef97ddfde
PayPeer <129145192+P2PayPeer@users.noreply.github.com>
2023-12-08 02:56:02 -0500

    🏟️ main.py

--- main.py
+++ main.py
@@ -54,6 +54,55 @@ def main():
         # Stream updates
         stream_integration.stream_update(virtual_arena, battle_round)
 
+#    # Main event loop [DEV: Leyley-13B]
+#    while True:
+#        # Display the menu
+#        print("Welcome to AI-ARENA!")
+#        print("1. Choose Characters")
+#        print("2. Choose Arena Type")
+#        print("3. Start Fight!")
+#        print("4. Exit")
+#        user_choice = int(input("Your choice: "))
+    
+#        # Process user input
+#        if user_choice == 1:
+#            character_choices = ["Onii-chan", "Ley-ley"]
+#            selected_characters = []
+#            while len(selected_characters) < 2:
+#                print("Select characters: ")
+#                for char in character_choices:
+#                    print(f"{char} | {character_choices.index(char)}")
+#                chosen_characters = [int(input(f"Enter number for '{ch}' (0 to dismiss): ")) for ch in character_choices]
+#                if chosen_characters[0] != 0 and chosen_characters[1] != 0:
+#                    selected_characters.extend([chosen_characters[0], chosen_characters[1]])
+#                elif chosen_characters[0] == 0:
+#                    print("Please select both characters.")
+#                elif chosen_characters[1] == 0:
+#                    print("Please select both characters.")
+#                else:
+#                    print("Invalid input.")
+    
+#            battle_round.set_fighters(selected_characters)
+            
+#        elif user_choice == 2:
+#            room_types = ["Bedroom", "Kitchen", "Living Room"]
+#            selected_rooms = []
+#            while len(selected_rooms) < 1:
+#                print("Choose arena types: ")
+#                for room in room_types:
+#                    print(f"{room} | {room_types.index(room)}")
+#                chosen_room = input("Enter number for the desired room: ")
+#                try:
+#                    selected_rooms.append(room_types.index(chosen_room))
+#                except ValueError:
+#                    print("Invalid input. Please enter a valid option.")
+    
+#            battle_round.set_arena(selected_rooms[0])
+#        elif user_choice == 3:
+#            virtual_arena.start_match()
+#        elif user_choice == 4:
+#            break
+    
     # Cleanup and exit
     logger.log("Shutting down AI-ARENA")
     virtual_arena.cleanup()
